f77a8897b921619b460ad78f01dbc32f345a9fb8
kenneth araniador <kenara19@gmail.com>
2016-07-30 12:48:13 +0800

    twitter and github
    working twitter and github authentication

--- github/github.js
+++ github/github.js
@@ -1,4 +1,21 @@
 angular.module("githubSignin", [])
-.controller("githubCtrl", ["$scope", function($scope){
-	
+.controller("githubCtrl", ["$scope", "authFire", function($scope, authFire){
+	$scope.githubPopup =function(){
+		authFire.$signInWithPopup("github").then(function(result){
+			console.log("Logged in: " + result.user.uid);
+		}).catch(function(error){
+			if(error.code === "auth/account-exists-with-different-credential") {
+				alert("email address already exist")	
+			}
+			console.log(error)
+			
+		})
+	};
+	$scope.githubRedirect = function (){
+		authFire.$signInWithRedirect("github").then(function(result){
+			console.log("Logged in as: " + result.user.uid)
+		}).catch(function(error){
+			console.log(error);
+		})
+	};
 }])
--- twitter/twitter.js
+++ twitter/twitter.js
@@ -1,4 +1,17 @@
 angular.module("twitterSignin", [])
-.controller("twitterCtrl", ["$scope", function($scope){
-	
+.controller("twitterCtrl", ["$scope", "authFire", function($scope, authFire){
+	$scope.twitterPopup =function(){
+		authFire.$signInWithPopup("twitter").then(function(result){
+			console.log("Logged in: " + result.user.uid);
+		}).catch(function(error){
+			console.log(error);
+		})
+	};
+	$scope.twitterRedirect = function (){
+		authFire.$singInWithRedirect("twitter").then(function(result){
+			console.log("Logged in as: " + result.user.uid)
+		}).catch(function(error){
+			console.log(error);
+		})
+	};
 }])
