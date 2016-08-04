6d0b5804212fee541c05ad27b72b5799696e077d
kenneth araniador <kenara19@gmail.com>
2016-08-04 15:56:30 +0800

    database posting
    saving to database post, user-post, user

--- database/database.js
+++ database/database.js
@@ -1,16 +1,28 @@
 angular.module("database", [])
-.controller("dataCtrl", ["$scope", "$firebaseArray", function($scope, $firebaseArray){
-	var useruid = firebase.auth().currentUser.uid;
-	var ref = firebase.database().ref("users/" + useruid);
-	var list = $firebaseArray(ref);
+.controller("dataCtrl", ["$scope", "$firebaseArray", "$firebaseObject", function($scope, $firebaseArray, $firebaseObject){
+	
 	
 	$scope.submitPost = function(){
-		var email = firebase.auth().currentUser.email;
-		list.$add({
-			email: email
-		}).then(function(data){
-			console.log(data.key)
-		})
-	}
+		var postRef = firebase.database().ref("posts");
+		var post = $firebaseArray(postRef);
+		post.$add({
+			title: $scope.title,
+			body: $scope.message
+		}).then(function(ref){
+			console.log("added post successfull" );
+			var postRefuser = firebase.database().ref("user-posts/" + firebase.auth().currentUser.uid + "/" + ref.key);
+			var postUser = $firebaseObject(postRefuser);
+			postUser.title = $scope.title;
+			postUser.message = $scope.message;
+			postUser.$save().then(function(ref){
+				console.log("added post to user success")
+			
+			}).catch(function(err){
+				console.log(err)
+			})
+				
+		});
+		
+	};
 }]);
 	
